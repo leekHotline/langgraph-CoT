e59422525e89fdcfdeb1c748a86abeea358573c1
leekHotline <117092932+leekHotline@users.noreply.github.com>
2026-01-05 00:44:17 +0800

    chore: support the code strength

--- src/main.py
+++ src/main.py
@@ -3,12 +3,13 @@ import dotenv
 from loguru import logger
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from typing import Optional
 from fastapi.responses import StreamingResponse
 from .cot.cot_agent import build_cot_graph
 import json
 
-
+# render默认端口加载于环境变量 默认10000
 
 app = FastAPI()
 cot_graph = build_cot_graph()
@@ -46,12 +47,20 @@ async def cot_api(question: str, stream_switch: Optional[bool] = None):
 def health_check():
     return {"message": "pong"}
 
+# 默认跟路由
+@app.get("/")
+def root_router():
+    return JSONResponse(content={
+        "message" : "Welcome to the CoT API Service, which is powered by Render!",
+    })
+
 if __name__ == "__main__":
     logger.info("Starting server at localhost:8000")
+    port = int(os.getenv("PORT", "8000")) # 环境变量没有 端口就为8000 字符串转整数
     uvicorn.run(
         app="src.main:app", # 需要导入模块
         host = "0.0.0.0", # 回环地址可以被外部访问
-        port = 8000, # 暴露的端口
+        port = port, # 暴露的端口
         reload = True, # 代码变动时自动重启
     )
 
